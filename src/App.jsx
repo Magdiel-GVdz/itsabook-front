2ce1a1452381a5d834a53886f17ed03380b67aea
Magdiel Guillermo Valdez <magdiel.gvdz@gmail.com>
2024-03-11 13:55:23 -0600

    test workflow 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Signup from './components/Signup'
-import UserPool from './UserPool'
 
 const App = () => {
   
